dac7c5c39607931588617280e9e0d7ee5b205ec6
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-25 19:21:16 -0500

    user model

---
+++ src/models/user.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowerCase: true,
+        match: /\S+@\S+\.\S+/,
+        index: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isVerified: {
+        type:Boolean,
+        default: false,
+    }
+}, {
+    timestamps: true,
+});
+
+// Password encryption 
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+    }
+    next();
+});
+
+// Generates JWT Token using user's info
+userSchema.methods.generateJWT = function() {
+    return jwt.sign({
+        id: this._id,
+        email: this.email,
+    }, process.env.JWT_SECRET, {expiresIn: '24h'});
+}
+
+
+export default mongoose.model('User', userSchema);
